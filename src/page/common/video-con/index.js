03f88141593cbbcca89e597fdac1e8e1382ba2ad
garyChen <1253634597@qq.com>
2017-08-19 04:07:54 +0800

    v1.0.2

--- src/page/common/video-con/index.js
+++ src/page/common/video-con/index.js
@@ -1,13 +1,13 @@
 require('./index.css')
 $('.video-item').mouseover(function(){
     var $this = $(this);
-    $this.children('.video-play').fadeIn('slow');
-    $this.css({'background-size':'220px 220px','opacity':'0.8'});
+    $this.children('.video-play').show();
+    $this.css({'backgroundSize':'220px 220px','opacity':'0.8'});
 });
 $('.video-item').mouseout(function(e){
     var $this = $(this);
     if(!(e.relatedTarget==$this[0]||e.relatedTarget==$this.children('.video-play')[0])){
-        $this.children('.video-play').fadeOut();
+        $this.children('.video-play').hide();
     }
-    $this.css({'background-size':'200px 200px','opacity':'1'});
+    $this.css({'backgroundSize':'200px 200px','opacity':'1'});
 });
